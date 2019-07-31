b734e9dbb03ecc1050ecb2285e2f25ec4c758c2e
censhuangshuang <1579362573@qq.com>
2019-07-31 14:49:16 +0800

    处理项目无法编译问题

--- Study/src/testNIO/Client.java
+++ Study/src/testNIO/Client.java
@@ -1,6 +1,30 @@
 package testNIO;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class Client {
 
+    public void start() throws IOException {
+        // 连接服务器
+        SocketChannel channel = SocketChannel.open(new InetSocketAddress("localhost", 8888));
 
+        // 向服务器请求数据
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String request = sc.nextLine();
+            if (request != null && request.length() > 0) {
+                channel.write(Charset.forName("utf-8").encode(request));
+            }
+        }
+
+        // 接收服务器的响应
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Client().start();
+    }
 }
--- Study/src/testNIO/Server.java
+++ Study/src/testNIO/Server.java
@@ -3,9 +3,11 @@ package testNIO;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Server {
 
@@ -18,30 +20,96 @@ public class Server {
         ServerSocketChannel channel = ServerSocketChannel.open();
         channel.bind(new InetSocketAddress(8888));
 
-        //（3）将Channel设置为非阻塞模式（最重要）
+        //（3）将Channel设置为非阻塞模式（最重要！）
         channel.configureBlocking(false);
 
         //（4）将Channel注册到Selector上，监听连接事件
         channel.register(selector, SelectionKey.OP_ACCEPT);
         System.out.println("----服务器启动成功----");
 
-        //（5）循环调用Selector的select()方法，检测就绪情况
+        //（5）循环调用Selector的select()方法，时刻检测就绪情况
         for (; ; ) {
-            int readyChannel = selector.select();
+            //获取可用channel的数量
+            int readyChannels = selector.select();
 
-            while (true) {
+            if (readyChannels == 0) continue;
 
+            //（6）调用selectedKeys()方法获取就绪channel集合
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+            Iterator iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = (SelectionKey) iterator.next();
+                iterator.remove();
+
+                //（7）判断就绪事件种类，调用业务处理方法
+                // 如果是接入事件
+                if (selectionKey.isAcceptable()) {
+                    acceptHandler(channel, selector);
+                }
+                // 如果是可读事件
+                if (selectionKey.isReadable()) {
+                    readHandler(selectionKey, selector);
+                }
             }
         }
-        //（6）调用selectedKeys()方法获取就绪channel集合
 
-        //（7）判断就绪事件种类，调用业务处理方法
 
         //（8）根据业务需要决定是否再次注册监听事件，重复执行第（3）步操作
     }
 
-    public static void main(String[] args){
+    /**
+     * 接入事件处理器
+     */
+    private void acceptHandler(ServerSocketChannel serverSocketChannel, Selector selector
+    ) throws IOException {
+
+        //创建SocketChannel
+        SocketChannel socketChannel = serverSocketChannel.accept();
+
+        // 将channel设置为非阻塞模式
+        socketChannel.configureBlocking(false);
+
+        //将channel注册到selector上，监听可读事件
+        socketChannel.register(selector, SelectionKey.OP_READ);
+
+        //响应客户端
+        socketChannel.write(Charset.forName("utf-8").encode("你已进入多人聊天室！请注意隐私安全！"));
+    }
+
+    /**
+     * 可读事件处理器
+     */
+    private void readHandler(SelectionKey selectionKey, Selector selector) throws IOException {
+
+        // 从SelectionKey中获取已就绪的channel
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+        // 创建buffer
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+        // 循环读取客户端请求信息
+        String request = "";
+        while (socketChannel.read(byteBuffer) > 0) {
+            // 切换至读模式
+            byteBuffer.flip();
+
+            // 读取buffer中的内容
+            request += Charset.forName("utf-8").decode(byteBuffer);
+        }
+
+        // 将channel再次注册到selector上，监听其他可读事件
+        socketChannel.register(selector, selectionKey.OP_READ);
+
+        // 将客户端发送的请求信息广播给其他客户端
+        if (request.length() > 0) {
+            System.out.println("-------"+request);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         Server server = new Server();
+        server.start();
     }
 
 
